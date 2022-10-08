43196230557df60383503eab14a0b5f8d6111f7b
Essayey <47187299+Essayey@users.noreply.github.com>
2022-10-08 21:11:28 +0300

    Add modal window to posts page

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AddModal from './AddModal';
 import PostItem from './PostItem'
 import PostList from './PostList'
 
 const Posts = () => {
+    const [postAdding, setPostAdding] = useState(false);
+
+
     return (
         <div className='Container'>
             <h1 style={{ textAlign: 'center' }}>Posts page</h1>
             <PostList />
+            <button onClick={() => setPostAdding(true)}>Add post</button>
+            {postAdding &&
+                <AddModal onHide={() => setPostAdding(false)} />}
         </div>
     )
 }
